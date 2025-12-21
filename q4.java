100e3aaf0b5f1573a82e2941ab6292eae2b3779b
Rishav <95118742+iamrishavv@users.noreply.github.com>
2025-12-21 15:08:11 +0530

    Add files via upload

---
+++ q4.java
@@ -0,0 +1,38 @@
+package Assignment;
+
+import java.util.Scanner;
+
+//wap to print day from taking input from the keyboard.
+public class q4 {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the num: ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        switch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid Input");
+        }
+    }
+}
