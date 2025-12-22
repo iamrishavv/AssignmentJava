aa7ccebe31956b5075031a67e6dc6270d875b797
Rishav <95118742+iamrishavv@users.noreply.github.com>
2025-12-22 17:51:56 +0530

    Add files via upload

--- q10.java
+++ q10.java
@@ -7,12 +7,18 @@ import java.util.Scanner;
 //descending order pattern
 public class q10 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+      //  int m=sc.nextInt();
         //pyramid
-        for(int i=1;i<=n;i++){
-            for()
+        for(int i=1;i<=5;i++){
+            for(int j=i;j< 5;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=(2*i-1);k++){
+                System.out.print("*");
+            }
+            System.out.println( );
         }
     }
 }
